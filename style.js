662c34555cce871f2950ec1bd175bdfe328a8d66
audreylizzy <lizzyaudreygideon@gmail.com>
2024-05-08 12:54:19 -0700

    styling

---
+++ style.js
@@ -0,0 +1,28 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var links = document.querySelectorAll('#menu a');
+//     var menu = document.getElementById('menu');
+    
+//     links.forEach(function(link) {
+//       link.addEventListener('click', function() {
+//         menu.style.display = 'none';
+//       });
+//     });
+//   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('#menu a');
+    var menu = document.getElementById('menu');
+    
+    links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        
+        menu.style.display = 'none';
+
+        window.scrollTo(0, 0);
+       
+        setTimeout(function() {
+          menu.style.display = '';
+        }, 5000);
+      });
+    });
+  });
